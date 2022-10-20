fab52412e9f66b43961162eb4f19c95a428d7dba
Compotation <46853874+Compotation@users.noreply.github.com>
2022-10-20 16:56:12 -0400

    fix task b

--- main.cpp
+++ main.cpp
@@ -7,14 +7,18 @@ int main() {
     while(std::getline(std::cin, currentLine)) {
         std::cout << removeLeadingSpaces(currentLine) << std::endl;
     }*/
+
     int indentLevel = 0;
     while(std::getline(std::cin, currentLine)) {
-        if (countChar(currentLine, '}') == 1) {
+        std::string unindentedLine = removeLeadingSpaces(currentLine);
+        bool firstCharClose = unindentedLine[0] == '}';
+        if (firstCharClose) {
             indentLevel--;
         }
-        std::cout << indentLine(removeLeadingSpaces(currentLine), indentLevel) << std::endl;
-        if (countChar(currentLine, '{') == 1) {
-            indentLevel++;
+        std::cout << indentLine(currentLine, indentLevel) << std::endl;
+        indentLevel += countChar(unindentedLine, '{');
+        if (!firstCharClose) {
+            indentLevel -= countChar(unindentedLine, '}');
         }
     }
     return 0;
