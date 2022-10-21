aa23e021e7f3e20d6684453ca6c79ad8f627714f
Compotation <46853874+Compotation@users.noreply.github.com>
2022-10-21 00:06:49 -0400

    put filename in separate var

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 #include <fstream>
 
 int main() {
+    const std::string FILENAME = "poorlyformatted.cpp";
     std::ifstream taskAFile;
-    taskAFile.open("poorlyformatted.cpp");
+    taskAFile.open(FILENAME);
     std::cout << "Task A" << std::endl << std::endl;
     unindentLines(taskAFile);
 
     std::ifstream taskBFile;
-    taskBFile.open("poorlyformatted.cpp");
+    taskBFile.open(FILENAME);
     std::cout << "Task B" << std::endl << std::endl;
     indentLines(taskBFile);
 }
